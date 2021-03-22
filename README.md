29f0849921640394158b243c70cce7527050490a
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-22 11:53:57 +0300

    Implement get-poe-status / get-port-statistics /  get-vlans

--- README.md
+++ README.md
@@ -24,6 +24,126 @@ Scraper was tested with the following  models:
 
 ## How
 
+Install:
+
+```bash
+pip3 install --requirement requirements.txt
+```
+
+Run:
+
 ```bash
 scraper.py --help
+```
+
+Examples:
+
+`scraper.py get-poe-status --host 10.200.199.198 --username admin --password admin --port 0 --output-format json | jq`:
+
+```json
+{
+  "type-a-state": "ON",
+  "type-b-state": "OFF",
+  "type-a-voltage": "47.92 V",
+  "type-b-voltage": "",
+  "type-a-current": "51 mA",
+  "type-b-current": "",
+  "type-a-power": "2.401 W",
+  "type-b-power": ""
+}
+```
+
+`scraper.py get-port-statistics --host 10.200.199.198 --username admin --password admin --port 0 --output-format json | jq`:
+
+```json
+{
+  "rx-good-bytes": 2860647484,
+  "tx-good-bytes": 2234770824,
+  "rx-bad-bytes": 0,
+  "tx-bad-bytes": null,
+  "rx-collision-packets": 0,
+  "tx-collision-packets": null,
+  "rx-discards-packets": 49859,
+  "tx-discards-packets": null,
+  "rx-filtered-packets": 0,
+  "tx-filtered-packets": 49166,
+  "rx-unicast-packets": 655319341,
+  "tx-unicast-packets": 2881055942,
+  "rx-broadcast-packets": 13765823,
+  "tx-broadcast-packets": 336705415,
+  "rx-multicast-packets": 16391,
+  "tx-multicast-packets": 24854558,
+  "rx-fcs-errors": 0,
+  "tx-fcs-errors": 0,
+  "rx-pause": 0,
+  "tx-pause": 0,
+  "rx-undersize": 0,
+  "tx-undersize": null,
+  "rx-oversize": 0,
+  "tx-oversize": null,
+  "rx-fragments": 0,
+  "tx-fragments": null,
+  "rx-jabber": 0,
+  "tx-jabber": null,
+  "rx-macrcverror": 0,
+  "tx-macrcverror": null,
+  "rx-deferred": null,
+  "tx-deferred": 0,
+  "rx-excessive": null,
+  "tx-excessive": 0,
+  "rx-single": null,
+  "tx-single": 0,
+  "rx-multiple": null,
+  "tx-multiple": 0
+}
+```
+
+`scraper.py get-vlans --host 10.200.199.198 --username admin --password admin --output-format json | jq`:
+
+```json
+[
+  {
+    "vid": 1,
+    "name": "Default",
+    "enabled": true,
+    "tagged-ports": [],
+    "untagged-ports": []
+  },
+  {
+    "vid": 8,
+    "name": "v8",
+    "enabled": true,
+    "tagged-ports": [
+      5,
+      6
+    ],
+    "untagged-ports": []
+  },
+  {
+    "vid": 76,
+    "name": "v76",
+    "enabled": true,
+    "tagged-ports": [
+      5,
+      6
+    ],
+    "untagged-ports": [
+      2,
+      3,
+      4
+    ]
+  },
+  {
+    "vid": 3504,
+    "name": "v3504",
+    "enabled": true,
+    "tagged-ports": [
+      5,
+      6
+    ],
+    "untagged-ports": [
+      1
+    ]
+  }
+]
 ```
--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 
 
 import click
+import json
 import re
 import requests
 from requests.auth import HTTPDigestAuth
@@ -14,53 +15,102 @@ def cli():
 
 
 @cli.command()
-@click.option('--host')
-@click.option('--username')
-@click.option('--password')
-def get_vlans(host, username, password):
-   url = 'http://%s/vlan/VLAN_8021q.shtml' % host
-   response = requests.post(url, auth=HTTPDigestAuth(username, password))
-   html = response.text
-   table_body = re.findall(REGEXP_VLANS, html)[0]
-   _process_table_body_vlans(table_body[1:])
+@click.option('--host', required=True)
+@click.option('--username', required=True)
+@click.option('--password', required=True)
+@click.option('--output-format', required=True, type=click.Choice(['json', 'python']))
+def get_vlans(host, username, password, output_format):
+    url = 'http://%s/vlan/VLAN_8021q.shtml' % host
+    response = requests.post(url, auth=HTTPDigestAuth(username, password))
+    html = response.text
+    table_body = re.findall(REGEXP_VLANS, html)[0]
+    result = _process_vlans(table_body[1:])
+    _print_result(result, output_format)
 
 
 @cli.command()
-@click.option('--host')
-@click.option('--username')
-@click.option('--password')
-@click.option('--port', type=int)
-def get_port_statistics(host, username, password, port):
-   url = 'http://%s/info/port_info.shtml?port=%d' % (host, port)
-   response = requests.post(url, auth=HTTPDigestAuth(username, password))
-   html = response.text
-   table_body = re.findall(REGEXP_PORT_STATISTICS, html)[0]
-   _process_table_body_port_info(table_body[1:])
+@click.option('--host', required=True)
+@click.option('--username', required=True)
+@click.option('--password', required=True)
+@click.option('--port', required=True, type=int)
+@click.option('--output-format', required=True, type=click.Choice(['json', 'python']))
+def get_port_statistics(host, username, password, port, output_format):
+    url = 'http://%s/info/port_info.shtml?port=%d' % (host, port)
+    response = requests.post(url, auth=HTTPDigestAuth(username, password))
+    html = response.text
+    table_body = re.findall(REGEXP_PORT_STATISTICS, html)[0]
+    result = _process_port_statistics(table_body[1:])
+    _print_result(result, output_format)
 
 
 @cli.command()
-@click.option('--host')
-@click.option('--username')
-@click.option('--password')
-@click.option('--port', type=int)
-def get_poe_status(host, username, password, port):
-   url = 'http://%s/info/port_info.shtml?port=%d' % (host, port)
-   response = requests.post(url, auth=HTTPDigestAuth(username, password))
-   html = response.text
-   table_body = re.findall(REGEXP_POE_STATUS, html)[0]
-   _process_table_body_port_info(table_body[1:])
+@click.option('--host', required=True)
+@click.option('--username', required=True)
+@click.option('--password', required=True)
+@click.option('--port', required=True, type=int)
+@click.option('--output-format', required=True, type=click.Choice(['json', 'python']))
+def get_poe_status(host, username, password, port, output_format):
+    url = 'http://%s/info/port_info.shtml?port=%d' % (host, port)
+    response = requests.post(url, auth=HTTPDigestAuth(username, password))
+    html = response.text
+    table_body = re.findall(REGEXP_POE_STATUS, html)[0]
+    result = _process_poe_status(table_body[1:])
+    _print_result(result, output_format)
 
 
-def _process_table_body_vlans(table_body):
+def _process_vlans(table_body):
+    result = []
     for r in re.findall(REGEXP_TR, table_body):
         d = re.findall(REGEXP_TD, r)
-        print(d[0], d[1], d[2], d[3], d[4])
+        result.append({
+            'vid': int(d[0]),
+            'name': d[2],
+            'enabled': bool(d[1]),
+            'tagged-ports': [int(x) for x in d[3].split()],
+            'untagged-ports': [int(x) for x in d[4].split()]
+        })
+    return result
 
 
-def _process_table_body_port_info(table_body):
+def _process_port_statistics(table_body):
+    result = {}
     for r in re.findall(REGEXP_TR, table_body):
         d = re.findall(REGEXP_TD, r)
-        print(d[0], d[1], d[2])
+        result[_normalize_key_name('rx %s' % d[0])] = _to_int(d[1])
+        result[_normalize_key_name('tx %s' % d[0])] = _to_int(d[2])
+    return result
+
+
+def _process_poe_status(table_body):
+    result = {}
+    for r in re.findall(REGEXP_TR, table_body):
+        d = re.findall(REGEXP_TD, r)
+        result[_normalize_key_name('type a %s' % d[0])] = d[1]
+        result[_normalize_key_name('type b %s' % d[0])] = d[2]
+    return result
+
+
+def _normalize_key_name(s):
+    return s \
+        .replace('  ', '-') \
+        .replace(' ', '-') \
+        .lower()
+
+
+def _to_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+
+def _print_result(result, output_format):
+    if output_format == 'python':
+        print(result)
+    elif output_format == 'json':
+        print(json.dumps(result))
+    else:
+        abort('Unknown output format')
 
 
 def _abort(message):
